f16c97e4f80c2477b2481ee3df90905fd56775e7
hh47521 <1421485266@qq.com>
2019-03-09 15:35:10 +0800

    静态首页面初步完成

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+//通过require()导入所需模块
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+
+//创建并发布任务
+gulp.task('index',function(){
+	gulp.src('./src/sass/index.scss')
+	.pipe(sass())
+	//.pipe(cssnano())
+	.pipe(rename({'suffix' : '.min'}))
+	.pipe(gulp.dest('./dist'));
+})
+gulp.task('default',()=>{
+	gulp.watch(['./src/sass/index.scss'],['index']);
+})
