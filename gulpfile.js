6fe8736c78cba6d4c470477c14786447c4a23f26
hh47521 <1421485266@qq.com>
2019-03-11 11:01:00 +0800

    首页初步完成

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('indexsass',function(){
 	.pipe(gulp.dest('./dist'));
 })
 gulp.task('default',()=>{
-	gulp.watch(['./src/sass/index.scss','./src/js/indexswiper.js'],['indexsass','indexswiper']);
+	gulp.watch(['./src/sass/index.scss','./src/js/indexswiper.js','./src/index.js'],['indexsass','indexswiper','index.js']);
 })
 gulp.task('indexswiper',function(){
 	gulp.src('./src/js/indexswiper.js')
@@ -25,8 +25,15 @@ gulp.task('indexswiper',function(){
 	.pipe(gulp.dest('./dist'));
 })
 
+gulp.task('default',function(){
+    gulp.src('./src/js/index.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('./src'))
+});
 gulp.task('indexjs',function(){
-	gulp.src('./src/js/index.js')
+	gulp.src('./src/index.js')
 	.pipe(uglify())
 	.pipe(rename({'suffix' : '.min'}))
 	.pipe(gulp.dest('./dist'));
